af88cb84237e3f0bcdacdb939f7a8e279d546866
poincaresolutions <r32.mcastillo@gmail.com>
2019-05-21 19:41:33 -0500

    enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -44,7 +44,7 @@ export const environment = {
 */
 export const environment = {
   production: false
-  , appVersion : 'Desarrollo V 0.0.0.1'
+  ,appVersion : 'Desarrollo V 1.0'
   , mnUrl      : 'http://localhost:1155/'  // micro catalago maestro
   , micro_catalago_maestro      : 'http://localhost:1155/'  // micro catalago maestro
   , sgUrl      : 'http://localhost:8400/'  // seguridad
@@ -54,4 +54,6 @@ export const environment = {
   , mtUrl      : 'http://localhost:1188/'  // micro tag
   , coUrl      : 'http://localhost:1199/'  // micro compliance seguimiento
   , estatus_maestro      : 'http://localhost:1177/'  // micro estatus maestro
+  ,securityUrl     : 'http://localhost:11122/security/'  // estatus maestro,
+  ,mercadoUrl     : 'http://localhost:11120/'  // estatus maestro
 };
