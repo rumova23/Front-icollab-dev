99d86f2ee4700578281cd94cf909f26bd3fcf9ce
poincaresolutions <r32.mcastillo@gmail.com>
2019-05-22 10:59:08 -0500

    environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -14,7 +14,7 @@ micro-compliance-seguimiento   1199   1200
 /*
 export const environment = {
   production: false
-  ,appVersion : 'Desarrollo V 0.0.0.1'
+  ,appVersion : 'Desarrollo V 1.0'
   ,sgUrl      : 'http://localhost:8400/'  //seguridad port:8400
   ,fdUrl      : 'http://localhost:1211/'  //filedb
   ,micro_catalago_maestro      : 'http://201.149.85.14:8383/catalogomaestro/'  //micro catalago maestro port:1155
@@ -23,7 +23,9 @@ export const environment = {
   ,cnUrl      : 'http://201.149.85.14:8383/evaluacionexamen/' //evaluacion port:1122
   ,mtUrl      : 'http://201.149.85.14:8383/tags/'  //micro tag port:1188
   ,coUrl      : 'http://201.149.85.14:8383/micro-seguimiento/'  //micro compliance seguimiento
-  ,estatus_maestro      : 'http://201.149.85.14:8383/estatus-maestro/'  // estatus maestro
+  ,estatus_maestro      : 'http://201.149.85.14:8383/estatus-maestro/'  // estatus maestr
+  ,securityUrl     : 'http://201.149.85.14:11122/security/'  // estatus maestro,
+  ,mercadoUrl     : 'http://201.149.85.14:11120/'  // estatus maestro
 };
 */
 /*  
