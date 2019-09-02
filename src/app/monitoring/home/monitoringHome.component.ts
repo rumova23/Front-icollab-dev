2a685507b2d005d8869128818c94d87cf1acf2a1
poincaresolutions <r32.mcastillo@gmail.com>
2019-09-02 12:38:24 -0500

    condicionar demonio de conexion a socket

--- src/app/monitoring/home/monitoringHome.component.ts
+++ src/app/monitoring/home/monitoringHome.component.ts
@@ -24,21 +24,118 @@ import { ThemeService } from 'src/app/core/globals/theme';
   ]
 })
 export class MonitoringHomeComponent implements OnInit, OnDestroy {
-  @Input() aside_open;
-  @ViewChild('container', { read: ViewContainerRef }) viewContainerRef: ViewContainerRef;
-  private subscriptions : Subscription[] = [];
-  private everySecond   : Observable<number> = timer(0,4000);
+	@Input() aside_open;
+	@ViewChild('container', { read: ViewContainerRef }) viewContainerRef: ViewContainerRef;
+	private subscriptions : Subscription[] = [];
+	private everySecond   : Observable<number> = timer(0,8000);
 
 
-  constructor(private route: ActivatedRoute,
-    public  globalService: GlobalService,
-    public  theme:ThemeService,
-    private componentFactoryResolver: ComponentFactoryResolver,
-    private eventService: EventService,
-    private securityService: SecurityService,
-    private socketService: SocketService
-   ) {
-		//console.log(">>>>>>> this.globalService.plantaDefaultId",this.globalService.plantaDefaultId);
+	constructor(
+		private route: ActivatedRoute,
+		public  globalService: GlobalService,
+		public  theme:ThemeService,
+		private componentFactoryResolver: ComponentFactoryResolver,
+		private eventService: EventService,
+		private securityService: SecurityService,
+		private socketService: SocketService
+	) {
+			this.subscribeOnMenu();
+			this.globalService.plant = this.securityService.loadPlants()[0];//tal vez sera bueno quitarla y
+			//ponerla directo en el home 
+			this.globalService.page  = 101; // dado que inicia con welcome
+	}
+	ngOnInit() {
+		this.openSocket();
+	}
+	ngAfterViewInit() {
+		const factory = this.componentFactoryResolver.resolveComponentFactory(MonitoringWelcomeComponent);
+		this.viewContainerRef.createComponent(factory);
+	}
+	ngOnDestroy(){
+		for (const iterator in this.subscriptions) {
+		this.subscriptions[iterator].unsubscribe();
+		}
+		this.socketService.closeSocket();
+	}
+	openSocket(){
+		if(!this.globalService.socketConnect){
+			const token = this.securityService.getToken();        
+			if (Validate(token)) {
+				let socket = this.socketService.initSocket(token);
+				if(socket["state"] == "open" || socket["state"] == "connecting"){
+					this.subscriptions['socketConnect']
+					=	this.socketService.onEvent(EventSocket.CONNECT)
+							.subscribe(() => {
+							this.unsubscribeOpenSocket();
+							this.globalService.socketConnect = true;
+							this.eventService.sendSocketConnect(new EventMessage(1, null));
+							console.log( "Socket Conectado::");
+						});
+					this.subscriptions['socketDisconnect']
+					= 	this.socketService.onEvent(EventSocket.DISCONNECT)
+						.subscribe(() => {
+							this.unsubscribeSocket();
+							this.subscribeOpenSocket();
+							this.globalService.socketConnect = false;
+							this.eventService.sendSocketConnect(new EventMessage(0, null));
+							console.log("Socket desconectado::");
+						});
+					this.subscriptions['socketOnError']
+					=   this.socketService.onError()
+						.subscribe((error: any) => {
+							this.unsubscribeSocket();
+							this.subscribeOpenSocket();
+							this.globalService.socketConnect = false;
+							this.eventService.sendSocketConnect(new EventMessage(0, null));
+							//console.log("Socket ERROR::",error);
+						});
+					
+							
+					this.subscriptions['socketLogin']
+					=   this.socketService.login()
+						.subscribe((errorLogin: any) => {
+							if (errorLogin) {
+								this.unsubscribeSocket();
+								this.subscribeOpenSocket();
+								this.globalService.socketConnect = false;
+								this.eventService.sendSocketConnect(new EventMessage(0, null));
+								//console.log("Socket ERROR Login::",errorLogin);
+							} else {
+								this.unsubscribeOpenSocket();
+								this.globalService.socketConnect = true;
+								this.eventService.sendSocketConnect(new EventMessage(1, null));
+							}
+						});
+						
+				}
+			}else {
+			console.log('Token inválido');
+			}
+		}
+	}
+	unsubscribeSocket(){
+		[  'socketConnect','socketDisconnect'
+			,'socketOnError','socketLogin'
+		].forEach(event=>{
+			if(this.subscriptions[event] != undefined && this.subscriptions[event].isStopped==false){
+			this.subscriptions[event].unsubscribe();
+			}
+		});
+	}
+	subscribeOpenSocket(){
+		if(this.subscriptions['openSocket']  == undefined || this.subscriptions["openSocket"].isStopped==true){
+			this.subscriptions['openSocket']
+			= this.everySecond.subscribe(second=>{
+				this.openSocket();
+			});
+		}
+	}
+	unsubscribeOpenSocket(){
+		if(this.subscriptions['openSocket']  != undefined && this.subscriptions["openSocket"].isStopped==false){
+			this.subscriptions['openSocket'].unsubscribe();
+		}
+	}
+	subscribeOnMenu(){
 		this.subscriptions.push(this.eventService.onChangeMainMonitoring.subscribe({
 			next: (event: EventMessage) => {
 			console.log("Click en Aside",event);
@@ -53,122 +150,40 @@ export class MonitoringHomeComponent implements OnInit, OnDestroy {
 			}
 			}
 		}));
-		  
-		this.globalService.plant = this.securityService.loadPlants()[0];
-		this.globalService.page  = 101; // dado que inicia con welcome
-  }
-  ngOnInit() {
-    this.subscriptions.push(this.everySecond.subscribe(second=>{
-      this.openSocket();
-    }));
-  }
-  ngOnDestroy(){
-    for (const iterator in this.subscriptions) {
-      this.subscriptions[iterator].unsubscribe();
-    }
-    this.socketService.closeSocket();
-  }
-  openSocket(){
-      if(!this.globalService.socketConnect){
-        const token = this.securityService.getToken();        
-        if (Validate(token)) {
-          let socket = this.socketService.initSocket(token);
-          if(socket["state"] == "open" || socket["state"] == "connecting"){
-              [  'socketConnect','socketDisconnect'
-                ,'socketOnError','socketLogin'
-              ].forEach(event=>{
-                if(this.subscriptions[event] != undefined && this.subscriptions[event].isStopped==false){
-                  this.subscriptions[event].unsubscribe();
-                }
-              });
-              
-              this.subscriptions['socketConnect'] =
-                this.socketService.onEvent(EventSocket.CONNECT)
-                  .subscribe(() => {
-                    this.globalService.socketConnect = true;
-                    /**
-                     * quitar proceso de reconeccion 
-                     */
-                    this.eventService.sendSocketConnect(new EventMessage(1, null));
-                    console.log( "Socket Conectado::" ,this.globalService.socketConnect);
-                  });
-              this.subscriptions['socketDisconnect'] =
-                this.socketService.onEvent(EventSocket.DISCONNECT)
-                  .subscribe(() => {
-                    this.globalService.socketConnect = false;
-                    /*
-                    quitar subscripciones 
-                    iniciar proceso de reconeccion cada x tiempo 
-                    */
-
-                    this.eventService.sendSocketConnect(new EventMessage(0, null));
-                    //console.log("Socket desconectado::",this.globalService.socketConnect);
-                  });
-              this.subscriptions['socketOnError'] =
-                this.socketService.onError()
-                  .subscribe((error: any) => {
-                    this.globalService.socketConnect = false;
-                    this.eventService.sendSocketConnect(new EventMessage(0, null));
-                    //console.log("Socket ERROR::",error);
-                });
-              
-                    
-              this.subscriptions['socketLogin'] =
-                this.socketService.login()
-                  .subscribe((errorLogin: any) => {
-                    if (errorLogin) {
-                      this.globalService.socketConnect = false;
-                      console.log("Socket ERROR Login::",errorLogin);
-                      this.eventService.sendSocketConnect(new EventMessage(0, null));
-                    } else {
-                      this.globalService.socketConnect = true;
-                      this.eventService.sendSocketConnect(new EventMessage(1, null));
-                    }
-                  });
-                
-          }
-        }else {
-           console.log('Token inválido');
-        }
-      }
-  }
-  getNameUser() {
-    let name = this.securityService.getNameUser() +" "+ this.securityService.getLastNameUser();
-    return name;
-  }
-  getgender(){
-    let generoId = JSON.parse(localStorage.getItem('user'));
-    generoId = generoId['generoId'];
-    return generoId;
-  }
-  ngAfterViewInit() {
-    const factory = this.componentFactoryResolver.resolveComponentFactory(MonitoringWelcomeComponent);
-    this.viewContainerRef.createComponent(factory);
-  }
-  private clickMenu(event: EventMessage): void {
-    this.viewContainerRef.clear();
-    switch (event.id) {
-      case 2:
-        this.viewContainerRef.createComponent(
-          this.componentFactoryResolver.resolveComponentFactory(MonitoringPhase2Component)
-        ).changeDetectorRef.detectChanges();
-      break;
-      case 3:
-        this.viewContainerRef.createComponent(
-          this.componentFactoryResolver.resolveComponentFactory(MonitoringPhase3Component)
-        ).changeDetectorRef.detectChanges();
-      break;
-      case 100:
-          this.viewContainerRef.createComponent(
-            this.componentFactoryResolver.resolveComponentFactory(ChangePasswordComponent)
-          ).changeDetectorRef.detectChanges();
-      break;
-      case 101:
-          this.viewContainerRef.createComponent(
-            this.componentFactoryResolver.resolveComponentFactory(MonitoringWelcomeComponent)
-          ).changeDetectorRef.detectChanges();
-      break;
-    }
-  }
+	}
+	private clickMenu(event: EventMessage): void {
+		this.viewContainerRef.clear();
+		switch (event.id) {
+		case 2:
+			this.viewContainerRef.createComponent(
+			this.componentFactoryResolver.resolveComponentFactory(MonitoringPhase2Component)
+			).changeDetectorRef.detectChanges();
+		break;
+		case 3:
+			this.viewContainerRef.createComponent(
+			this.componentFactoryResolver.resolveComponentFactory(MonitoringPhase3Component)
+			).changeDetectorRef.detectChanges();
+		break;
+		case 100:
+			this.viewContainerRef.createComponent(
+				this.componentFactoryResolver.resolveComponentFactory(ChangePasswordComponent)
+			).changeDetectorRef.detectChanges();
+		break;
+		case 101:
+			this.viewContainerRef.createComponent(
+				this.componentFactoryResolver.resolveComponentFactory(MonitoringWelcomeComponent)
+			).changeDetectorRef.detectChanges();
+		break;
+		}
+	}
+	getNameUser() {
+		let name = this.securityService.getNameUser() +" "+ this.securityService.getLastNameUser();
+		return name;
+	}
+	getgender(){
+		let generoId = JSON.parse(localStorage.getItem('user'));
+		generoId = generoId['generoId'];
+		return generoId;
+	}
 }
 
