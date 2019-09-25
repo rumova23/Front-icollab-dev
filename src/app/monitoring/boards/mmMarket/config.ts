17b08c61488c5f4761e35cfcc48cd9bfb16ff22e
Your Name <you@example.com>
2019-09-24 19:07:57 -0500

    colores , quitar boton

--- src/app/monitoring/boards/mmMarket/config.ts
+++ src/app/monitoring/boards/mmMarket/config.ts
@@ -1,7 +1,4 @@
 
-var getvalue=function(calltags,plant){
-    return this.lstTags;
-}
 export var listCharts = {
     mmMarket:{
         controls:{},
@@ -10,7 +7,7 @@ export var listCharts = {
     }
 }
 export var lstTags = [];
-lstTags['PPA']                     = {  label: 'PPA'                        ,min:400  ,max:700   ,stepSize:20 ,color:'#c56c49'  ,aguila:[{WebId:'P0uQAgHoBd0ku7P3cWOJL6IgGCUAAAU0VSVklET1JfUElcREFBMDgxMDM',WebTag:null}]  ,sol:[{WebId:'F1DP4rhZAwFMREKDf7s8vylUqgkgYAAAUElUVlxULkxHUy5DRUEuNDY'   ,WebTag:null}]  ,overview:[{value:null,timestamp:null}]};
-lstTags['CapacidadExcedente']      = {  label: 'Capacidad Excedente'        ,min:400  ,max:700   ,stepSize:20 ,color:'#008080'  ,aguila:[{WebId:'P0uQAgHoBd0ku7P3cWOJL6IgmCQAAAU0VSVklET1JfUElcUDJBMDgyMTI',WebTag:null}]  ,sol:[{WebId:'F1DP4rhZAwFMREKDf7s8vylUqgtgIAAAUElUVlw1MlBDLUFJLTAwNA'    ,WebTag:null}]  ,overview:[{value:null,timestamp:null}]};
-lstTags['PotenciaRealDemostrada']  = {  label: 'Potencia Real Demostrada'   ,min:400  ,max:700   ,stepSize:20 ,color:'#cd5294'  ,id_EAT:0  ,webId_EAT:'P0uQAgHoBd0ku7P3cWOJL6IgGyUAAAU0VSVklET1JfUElcREFBMDgxMDY'   ,id_EST:0   ,webId_EST:'F1DP4rhZAwFMREKDf7s8vylUqg3gMAAAUElUVlxULkNFQS4yMjcx'      };
+lstTags['PPA']                     = {  label: 'PPA'                        ,min:400  ,max:700   ,stepSize:20 ,color:'#00ff7f'  };
+lstTags['CapacidadExcedente']      = {  label: 'Capacidad Excedente'        ,min:400  ,max:700   ,stepSize:20 ,color:'#ffffff'  };
+lstTags['PotenciaRealDemostrada']  = {  label: 'Potencia Real Demostrada'   ,min:400  ,max:700   ,stepSize:20 ,color:'#ff0080'  };
 
