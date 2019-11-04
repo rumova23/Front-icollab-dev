467f676c2e158b5137f59375cbf6d936aa4aa1f4
poincaresolutions <r32.mcastillo@gmail.com>
2019-11-04 13:54:58 -0600

    clean

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,17 +1,18 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, Validators, NgForm } from '@angular/forms';
-import { environment } from 'src/environments/environment';
-import { AlertService } from 'src/app/core/services/alert.service';
-import { SecurityService } from 'src/app/core/services/security.service';
-import { EventService } from 'src/app/core/services/event.service';
-import { EventMessage } from 'src/app/core/models/EventMessage';
-import { EventBlocked } from 'src/app/core/models/EventBlocked';
-import { Validate } from 'src/app/core/helpers/util.validator.';
-import { GlobalService }                       from 'src/app/core/globals/global.service';
-import { App } from 'src/app/security/models/App';
-import { ThemeService } from 'src/app/core/globals/theme';
-import * as dis                           from './js/animations';
+import { Component, OnInit, OnDestroy                } from '@angular/core';
+import { Router                                      } from '@angular/router';
+import { FormBuilder, FormGroup, Validators          } from '@angular/forms';
+import { AlertService                                } from 'src/app/core/services/alert.service';
+import { SecurityService                             } from 'src/app/core/services/security.service';
+import { EventService                                } from 'src/app/core/services/event.service';
+import { EventMessage                                } from 'src/app/core/models/EventMessage';
+import { EventBlocked                                } from 'src/app/core/models/EventBlocked';
+import { GlobalService                               } from 'src/app/core/globals/global.service';
+import { ThemeService                                } from 'src/app/core/globals/theme';
+import { Validate                                    } from 'src/app/core/helpers/util.validator.';
+import { App                                         } from 'src/app/security/models/App';
+import { Subscription                                } from 'rxjs';
+import { ToastrManager                               } from 'ng6-toastr-notifications';
+import * as dis                                        from './js/animations';
 
 @Component({
 	selector    : 'app-login',
@@ -19,36 +20,27 @@ import * as dis                           from './js/animations';
 	styleUrls   : ['./css/style.css','./css/form.css']
 })
 export class LoginComponent implements OnInit,OnDestroy {
+	private subscriptions        : Subscription[]     = [];
+	private loginForm            : FormGroup;
+	public  apps                 : Array<App>;
 
-	apps: Array<App>;
-	loginForm: FormGroup;
-	loading = false;
-	submitted = false;
-	returnUrl: string;
-	appVersion = environment.appVersion;
-
-	constructor(private formBuilder: FormBuilder,
-		private route: ActivatedRoute,
-		private router: Router,
-		private securityService: SecurityService,
-			public  globalService            : GlobalService,
-		private eventService: EventService,
-			public  theme                    : ThemeService,
-		private alertService: AlertService) {
-
-		
-
-	}
+	constructor(
+		private formBuilder     : FormBuilder,
+		private router          : Router,
+		private securityService : SecurityService,
+		public  globalService   : GlobalService,
+		private eventService    : EventService,
+		public  theme           : ThemeService,
+		public  toastr          : ToastrManager,
+		private alertService    : AlertService
+	){}
 
 	ngOnInit() {
-
-
+		localStorage.clear();
 		this.loginForm = this.formBuilder.group({
 			usr  : ['', Validators.required],
 			pass : ['', Validators.required]
 		});
-		this.returnUrl = this.route.snapshot.queryParams.returnUrl || '/home';
-		
 		this.loadApps();
 
 		dis.disenio();
@@ -57,6 +49,9 @@ export class LoginComponent implements OnInit,OnDestroy {
 
 	ngOnDestroy(): void {
 		window.onscroll = function() {}; 
+		for (const iterator in this.subscriptions) {
+			this.subscriptions[iterator].unsubscribe();
+		}
 	}
 
 	algo(){
@@ -83,8 +78,6 @@ export class LoginComponent implements OnInit,OnDestroy {
 	}
 
 	existApp(name: string) {
-		//return false;
-		let fdafdsa = this.apps;
 		if(this.apps){
 			return Validate(this.apps.filter(app => app.name === name)[0]);
 		}
@@ -94,35 +87,30 @@ export class LoginComponent implements OnInit,OnDestroy {
 		window.onscroll = function() {}; 
 		this.router.navigate([uri]);
 	}
-	// convenience getter for easy access to form fields
-
-	
 	onSubmit() {
-		console.log(this.loginForm.value);
-		//this.addBlock(1, null);
-		this.submitted = true;
-		// stop here if form is invalid
+		//this.addBlock(1, null); //cargando
+		//this.addBlock(2, null); //desbloquear
 		if (this.loginForm.invalid) {
-			//this.addBlock(2, null);
+			this.toastr.errorToastr('Los datos son obligatorios', '');
 			return;
 		}
 
 		const loginData = this.loginForm.value;
 		
-		this.securityService.login({
-			user: loginData.usr,
-			password: loginData.pass })
-		.subscribe(
-			data => {
-				localStorage.setItem("user", JSON.stringify(data));
-				this.loading = true;
-				this.loadApps();
-				this.globalService.setPlant(this.securityService.loadPlants()[0]);
-				dis.next();
-			},
-			errorData => {
-				console.log(errorData);
-			});
+		this.subscriptions.push(
+			this.securityService.login({user: loginData.usr,password: loginData.pass })
+			.subscribe(
+				data => {
+					localStorage.setItem("user", JSON.stringify(data));
+					this.loadApps();
+					this.globalService.setPlant(this.securityService.loadPlants()[0]);
+					dis.next();
+				},
+				errorData => {
+					console.log(errorData);
+				}
+			)
+		);
 
 	}
 
