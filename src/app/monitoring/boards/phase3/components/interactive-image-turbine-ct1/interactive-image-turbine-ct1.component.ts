6f52b36678110a9d3e2145b5b6387f68f52db89f
Your Name <you@example.com>
2019-11-12 18:50:17 -0600

    avance popUp

--- src/app/monitoring/boards/phase3/components/interactive-image-turbine-ct1/interactive-image-turbine-ct1.component.ts
+++ src/app/monitoring/boards/phase3/components/interactive-image-turbine-ct1/interactive-image-turbine-ct1.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { element } from 'protractor';
 declare var $: any;
 
 @Component({
@@ -7,7 +8,8 @@ declare var $: any;
   styleUrls   : ['./interactive-image-turbine-ct1.component.scss']
 })
 export class InteractiveImageTurbineCT1Component implements OnInit {
-
+	@Input() lstTags: any;
+	public Tag_info = "";
 	constructor() { }
 
 	ngOnInit() {
@@ -16,24 +18,24 @@ export class InteractiveImageTurbineCT1Component implements OnInit {
 	openModalCt_1(){
 		$('#ModalTurbineCT1').modal('show');
 	}
-	animar(){
+	animar(idElement){
 		let element = document.getElementById('my-popup-info');
 		
 		//const element =  document.querySelector('.my-popup-info');
-		if(element.classList.contains('flipOutX')){
-			element.classList.remove('animated', 'flipOutX');
-
-			this.animateCSS('.my-popup-info','flipInX',()=>{
+		if(element.classList.contains('zoomOut')){
+			element.classList.remove('animated', 'zoomOut');
+			this.setInfo(idElement);
+			this.animateCSS('.my-popup-info','slideInDown',()=>{
 			
 			});
-		} else if(element.classList.contains('flipInX')){
+		} else if(element.classList.contains('slideInDown')){
 			
-			element.classList.remove('animated', 'flipInX');
+			element.classList.remove('animated', 'slideInDown');
 
-			this.animateCSS('.my-popup-info','flipOutX',()=>{
-			
-				element.classList.remove('animated', 'flipOutX');
-				this.animateCSS('.my-popup-info','flipInX',()=>{
+			this.animateCSS('.my-popup-info','zoomOut',()=>{
+				this.setInfo(idElement);
+				element.classList.remove('animated', 'zoomOut');
+				this.animateCSS('.my-popup-info','slideInDown',()=>{
 			
 				});
 			});
@@ -55,5 +57,31 @@ export class InteractiveImageTurbineCT1Component implements OnInit {
 		}
 	
 		node.addEventListener('animationend', handleAnimationEnd);
+	}
+	tag(idElement){
+		this.animar(idElement);
+		this.aplicarCheck(idElement);
+		console.log(this.lstTags);
+		
+	}
+	aplicarCheck(idElement:any){
+	  let selectores:any = document.getElementsByClassName("tagpoint");
+	  for (const ref of selectores) {
+		ref.classList.remove("active");
+	  }
+	  document.getElementById(idElement).classList.add("active");
+	}
+	setInfo(idElement){
+		switch (idElement) {
+			case 'tag_0':
+					this.Tag_info = "Hola mundo";
+				break;
+		
+			case 'tag_1':
+					this.Tag_info = "tag 1";
+				break;
+			default:
+				break;
+		}
 	}
 }
