72ef10f46e1ecdba1af3532f4efb230ff0fa1baf
Your Name <you@example.com>
2019-11-12 19:33:10 -0600

    Demos popup

--- src/app/monitoring/boards/phase3/components/interactive-image-turbine-ct1/interactive-image-turbine-ct1.component.ts
+++ src/app/monitoring/boards/phase3/components/interactive-image-turbine-ct1/interactive-image-turbine-ct1.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { element } from 'protractor';
+import { Chart } from 'chart.js';
 declare var $: any;
 
 @Component({
@@ -13,6 +14,81 @@ export class InteractiveImageTurbineCT1Component implements OnInit {
 	constructor() { }
 
 	ngOnInit() {
+		var myChart = new Chart('chart_rt5', {
+			type: 'line',
+			data: {
+				labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+				datasets: [{
+					label: '# of Votes',
+					data: [12, 19, 3, 5, 2, 3],
+					backgroundColor: [
+						'rgba(255, 99, 132, 0.2)',
+						'rgba(54, 162, 235, 0.2)',
+						'rgba(255, 206, 86, 0.2)',
+						'rgba(75, 192, 192, 0.2)',
+						'rgba(153, 102, 255, 0.2)',
+						'rgba(255, 159, 64, 0.2)'
+					],
+					borderColor: [
+						'rgba(255, 99, 132, 1)',
+						'rgba(54, 162, 235, 1)',
+						'rgba(255, 206, 86, 1)',
+						'rgba(75, 192, 192, 1)',
+						'rgba(153, 102, 255, 1)',
+						'rgba(255, 159, 64, 1)'
+					],
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero: true
+						},
+						position: 'right',
+					}]
+				}
+			}
+		});
+		
+		
+		var myChart = new Chart('chart_rt2', {
+			type: 'bar',
+			data: {
+				labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+				datasets: [{
+					label: '# of Votes',
+					data: [12, 19, 3, 5, 2, 3],
+					backgroundColor: [
+						'rgba(255, 99, 132, 0.2)',
+						'rgba(54, 162, 235, 0.2)',
+						'rgba(255, 206, 86, 0.2)',
+						'rgba(75, 192, 192, 0.2)',
+						'rgba(153, 102, 255, 0.2)',
+						'rgba(255, 159, 64, 0.2)'
+					],
+					borderColor: [
+						'rgba(255, 99, 132, 1)',
+						'rgba(54, 162, 235, 1)',
+						'rgba(255, 206, 86, 1)',
+						'rgba(75, 192, 192, 1)',
+						'rgba(153, 102, 255, 1)',
+						'rgba(255, 159, 64, 1)'
+					],
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero: true
+						},
+					}]
+				}
+			}
+		});
 	}
 
 	openModalCt_1(){
@@ -65,11 +141,11 @@ export class InteractiveImageTurbineCT1Component implements OnInit {
 		
 	}
 	aplicarCheck(idElement:any){
-	  let selectores:any = document.getElementsByClassName("tagpoint");
-	  for (const ref of selectores) {
-		ref.classList.remove("active");
-	  }
-	  document.getElementById(idElement).classList.add("active");
+		let selectores:any = document.getElementsByClassName("tagpoint");
+		for (const ref of selectores) {
+			ref.classList.remove("active");
+		}
+		document.getElementById(idElement).classList.add("active");
 	}
 	setInfo(idElement){
 		switch (idElement) {
