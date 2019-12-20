160626235facfd5d928dd4349852aa55fe0ad132
poincaresolutions <r32.mcastillo@gmail.com>
2019-12-20 13:03:33 -0600

    :alien:  loader

--- src/assets/js/js.js
+++ src/assets/js/js.js
@@ -1,8 +1,16 @@
 
 window.onload = function() {
-    var node= document.getElementById("loader");
-    if (node.parentNode) {
-        node.parentNode.removeChild(node);
-    }
+    setTimeout(function(){ 
+        var loader= document.getElementById("my00loader");
+        var mensaje= document.getElementById("my00msn-loader");
+        if (loader.parentNode) {
+            loader.parentNode.removeChild(loader);
+        }
+        if (mensaje.parentNode) {
+            mensaje.parentNode.removeChild(mensaje);
+        }
+        
+        document.getElementById("app-root").style.visibility = "visible"; 
 
+    }, 1000);
 };
