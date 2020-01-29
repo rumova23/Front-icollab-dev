6eadb49cf966104a0d0afb5eead20b4787acf543
Ricardo Torres Cano <rtc@iMac.local>
2020-01-28 19:08:48 -0600

    RTC: avance safe facturacion

--- src/app/safe/admin/fuecd/fuecd.component.ts
+++ src/app/safe/admin/fuecd/fuecd.component.ts
@@ -12,6 +12,9 @@ import { CatalogOrder } from 'src/app/core/models/CatalogOrder';
 import { CatalogOrderFind } from 'src/app/core/models/CatalogOrderFind';
 import { CatalogService } from 'src/app/core/services/catalog.service';
 import { Fuecd } from '../../models/Fuecd';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {TimeRegister} from '../../models/TimeRegister';
+import {requiredFileType} from '../../../core/helpers/requiredFileType';
 
 
 @Component({
@@ -21,14 +24,21 @@ import { Fuecd } from '../../models/Fuecd';
 })
 
 export class FuecdComponent implements OnInit {
+  fuecdForm: FormGroup;
+  progress;
+  file: any;
+  fileName: any;
+  valid = false;
+  timeRegisters: Array<TimeRegister> = [];
+
   loading: boolean;
   cols: any[];
   filters = [
-    { label: "Actividad", inputtype: "text" },
-    { label: "Prefijo", inputtype: "text" },
-    { label: "Activo", inputtype: "text" },
+    { label: 'Actividad', inputtype: 'text' },
+    { label: 'Prefijo', inputtype: 'text' },
+    { label: 'Activo', inputtype: 'text' },
   ];
-  filterBtn = { label: "buscar" };
+  filterBtn = { label: 'buscar' };
   rowsPorPage = [50, 100, 250, 500];
   fuecd: Array<Fuecd>;
   constructor(
@@ -37,11 +47,14 @@ export class FuecdComponent implements OnInit {
     public toastr: ToastrManager,
     private eventService: EventService,
     public globalService: GlobalService,
+    private fb: FormBuilder
   ) {
   }
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   ngOnInit() {
+    this.fuecdForm = this.fb.group({file: new FormControl(null, [Validators.required, requiredFileType('xml')])
+    });
     this.getFuecds();
     this.cols = [
       'id',
@@ -49,9 +62,9 @@ export class FuecdComponent implements OnInit {
       'competitorKey',
       'subcuentKey',
       'dateOperation',
-      "dateEmission",
-      "systemKey",
-      "invoice"
+      'dateEmission',
+      'systemKey',
+      'invoices'
     ];
     this.loading = false;
   }
@@ -78,7 +91,74 @@ export class FuecdComponent implements OnInit {
 
   invoice(fuecd) {
     this.eventService.sendMainSafe(new
-      EventMessage(24, { readOnly: false, edit: false, new: true, fuecd: fuecd }));
+      EventMessage(24, { readOnly: false, edit: false, new: true, fuecd }));
   }
 
+  validate(value) {
+    this.valid = false;
+    const reader = new FileReader();
+    reader.onloadend = (e) => {
+      this.file = reader.result;
+      this.file = this.file.replace(/^data:(.*;base64,)?/, '');
+
+      this.file = this.file.trim();
+      this.fileName = value.file.name;
+      this.marketService.validateFuecd({ file: this.file, name:  this.fileName})
+          .subscribe(data => {
+            console.log('validateFuecd');
+              console.dir(data);
+                console.log('validateFuecd');
+                const status = data;
+                this.save();
+                /*for (let a = 0; a < status.settlements.length; a++) {
+                  const settlement = status.settlements[a];
+                  for (let b = 0; b < settlement.settlementInvoices.length; b++) {
+                    const settlementInvoice = settlement.settlementInvoices[b];
+
+                    for (let c = 0; c < settlementInvoice.concepts.length; c++) {
+                      const concept = settlementInvoice.concepts[c];
+
+                      const timer: TimeRegister = {};
+                      timer.fuecd = status.fuecd;
+                      timer.concept = concept.ful;
+                      timer.date = settlementInvoice.datePayment;
+                      timer.ful = concept.ful;
+                      timer.concept = concept.description;
+                      timer.iva = concept.iva;
+                      timer.totalAmount = concept.totalAmount;
+                      timer.totalNet = concept.totalNet;
+                      this.timeRegisters.push(timer);
+                      for (let d = 0; d < concept.annexeds.length; d++) {
+                        const annexed = concept.annexeds[d];
+
+                        for (let e = 0; e < annexed.timeRegisters.length; e++) {
+                          const timeRegister = annexed.timeRegisters[e];
+                        }
+                      }
+                    }
+                  }
+                }*/
+                this.valid = true;
+              },
+              errorData => {
+                this.fuecdForm.reset();
+                this.toastr.errorToastr(Constants.ERROR_LOAD, errorData.error.message);
+              });
+    };
+    reader.readAsDataURL(value.file);
+  }
+  save() {
+    this.marketService.saveFuecd({ file: this.file, name: this.fileName })
+        .subscribe(
+            data => {
+              this.ngOnInit();
+            },
+            errorData => {
+              this.fuecdForm.reset();
+              this.valid = false;
+              this.timeRegisters = [];
+              this.toastr.errorToastr(Constants.ERROR_SAVE, errorData);
+            });
+
+  }
 }
