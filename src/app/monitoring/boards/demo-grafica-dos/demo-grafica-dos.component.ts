60ce73e465a276e8cddad450afa62b9f5f7ed385
poincaresolutions <r32.mcastillo@gmail.com>
2020-02-06 10:32:24 -0600

    :alien:  test henryhub socket

--- src/app/monitoring/boards/demo-grafica-dos/demo-grafica-dos.component.ts
+++ src/app/monitoring/boards/demo-grafica-dos/demo-grafica-dos.component.ts
@@ -3,21 +3,31 @@ import { GlobalService } from 'src/app/core/globals/global.service';
 import { ThemeService } from 'src/app/core/globals/theme';
 import { Chart } from 'chart.js';
 import { GraficaDosaService } from './service/grafica-dosa.service';
+import { MonitoringChartTR } from '../../class/monitoringChartTR.component';
+import { EventService } from 'src/app/core/services/event.service';
+import { SocketService } from 'src/app/core/services/socket.service';
+import { MonitoringTrService } from '../../services/monitoringTr.service';
+import { PiServerBox } from '../../models/piServer/piServerBox';
 
 @Component({
   selector: 'app-demo-grafica-dos',
   templateUrl: './demo-grafica-dos.component.html',
   styleUrls: ['./demo-grafica-dos.component.scss']
 })
-export class DemoGraficaDosComponent  implements OnInit {
+export class DemoGraficaDosComponent extends MonitoringChartTR implements OnInit {
 	@ViewChild('canvas1') canvas1:ElementRef;
 	charts = [];
 
 	constructor(
 		public globalService       : GlobalService,
 		public theme               : ThemeService,
-		private graficaDosaService : GraficaDosaService
+		private graficaDosaService : GraficaDosaService,
+		
+        public eventService        : EventService,
+        public socketService       : SocketService,
+        public monitoringTrService : MonitoringTrService
 	) {
+        super(globalService, eventService, socketService, monitoringTrService);
 	}
 
 	ngOnInit() {
@@ -35,6 +45,7 @@ export class DemoGraficaDosComponent  implements OnInit {
 				);
 			}
 		);
+		this.subscribeSocketChanelAguila();
 	}
 	chartCreateConfigDemo(data:any[]){
 		return{
@@ -67,4 +78,23 @@ export class DemoGraficaDosComponent  implements OnInit {
 		};
 	}
 
+	
+	subscribeSocketChanelAguila(){
+		let strChannel    : string = "henryhub"
+		let strChannelErr : string = `${strChannel}-error`;
+		let channel       = this.socketService.suscribeChannel(strChannel);
+
+
+		this.addSubscriptionsPerChannel([strChannel, strChannelErr]);
+		this.addChanels([strChannel]);
+
+		this.subscriptions[strChannelErr] = this.socketService.onChannelError(channel - 1).subscribe((errorChannel: any) => {console.log(strChannelErr,errorChannel);});
+		this.subscriptions[strChannel]    = this.socketService.onChannelWatch(channel - 1)
+		.subscribe((data) => {
+			this.dataAdapter(data);
+		});
+	}
+	dataAdapter(data:any){
+		debugger;
+	}
 }
