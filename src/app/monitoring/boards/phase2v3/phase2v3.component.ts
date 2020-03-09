dba3efefa380e6d8c67fcbfe42db6d1e2cd7d093
Your Name <you@example.com>
2020-03-08 18:31:59 -0600

    :alien:

--- src/app/monitoring/boards/phase2v3/phase2v3.component.ts
+++ src/app/monitoring/boards/phase2v3/phase2v3.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-phase2v3',
-  templateUrl: './phase2v3.component.html',
-  styleUrls: ['./phase2v3.component.scss']
+	selector: 'app-phase2v3',
+	templateUrl: './phase2v3.component.html',
+	styleUrls: ['./phase2v3.component.scss']
 })
 export class Phase2v3Component implements OnInit {
+	expected={
+		eat:{
+			powerOutput:440.55,
+			capacityFactor:89
+		},
+		est:{
+			powerOutput:455.4,
+			capacityFactor:92
+		}
+	}
+	table = {
+		overview:[
+			{
+				tag:"power",
+				value:{
+				actuals:{value:5,max:6},
+				dif:{value:5,max:6},
+				expected:{value:5,max:6}
+				}
+			}
+		],
+		est:[
+			{
+				tag:"power",
+				value:{
+				actuals:{value:5,max:6},
+				dif:{value:5,max:6},
+				expected:{value:5,max:6}
+				}
+			}
+		],
+		eat:[
+			{
+				tag:"power",
+				value:{
+				actuals:{value:5,max:6},
+				dif:{value:5,max:6},
+				expected:{value:5,max:6}
+				}
+			}
+		]
+	};
+	constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
