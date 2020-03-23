4b19db44b655bb67db57fbe73522fdf6ee03fc16
Your Name <you@example.com>
2020-03-23 13:12:29 -0600

    :alien:  Demo

--- src/app/safe/admin/safe-ppamonitoring-station/safe-ppamonitoring-station.component.ts
+++ src/app/safe/admin/safe-ppamonitoring-station/safe-ppamonitoring-station.component.ts
@@ -115,7 +115,7 @@ export class SafePPAMonitoringStationComponent implements OnInit {
 		},
 		series: [{
 			name: 'Rainfall',
-			type: 'column',
+			type: 'spline',
 			yAxis: 1,
 			data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
 			tooltip: {
