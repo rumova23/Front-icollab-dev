1d80b86e99b93805e6e8d60ab091d047a5696112
Your Name <you@example.com>
2020-05-04 20:16:21 -0500

    :alien:   Property 'ver' does not exist on type 'EditEvaluationHomeComponent'

--- src/app/compliance/business/competence/staff/evaluation/editevaluation/editEvaluationHome.component.ts
+++ src/app/compliance/business/competence/staff/evaluation/editevaluation/editEvaluationHome.component.ts
@@ -11,6 +11,7 @@ export class EditEvaluationHomeComponent implements OnInit {
   title = 'Competencia de los Recursos / Evaluación de Competencia para el Personal / Evaluación';
   idEmpleado: number;
   tipo: string;
+  ver:string;
   isViewable = true;
   isdisabled = false;
   isdisabledFinishBehavior = false;
