58299a33bcefa75c646eb252dd1b4043aede4ec1
Your Name <you@example.com>
2020-05-27 19:12:30 -0500

    :alien:  component table terminado

--- src/app/common/tables/mat-table/mat-table.component.ts
+++ src/app/common/tables/mat-table/mat-table.component.ts
@@ -1,76 +1,73 @@
-import { Component, OnInit, ViewChild, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, ViewChild, Input, Output, EventEmitter, SimpleChanges, OnChanges } from '@angular/core';
 import { MatTableDataSource, MatSort, MatPaginator, Sort } from '@angular/material';
+import { ColumnLabel } from '../../../core/models/ColumnLabel';
 @Component({
 	selector: 'app-mat-table',
 	templateUrl: './mat-table.component.html',
 	styleUrls: ['./mat-table.component.scss']
 })
-export class MatTableComponent implements OnInit {
+export class MatTableComponent implements OnInit , OnChanges {
     @ViewChild(MatPaginator) paginator: MatPaginator;
 	@ViewChild(MatSort) sort: MatSort;
 	@Output() clickSee    = new EventEmitter<any>();
 	@Output() clickEdit   = new EventEmitter<any>();
 	@Output() clickDelete = new EventEmitter<any>();
 	
-	
-	
-	
 	dataSource;
 	displayedColumnsActions: any[] = [
-		{ key: 'sys_see', label: 'See' },
-		{ key: 'sys_edit', label: 'Edit' },
-		{ key: 'sys_delete', label: 'Delete' }
+		{ key: 'sys_see'    , label: 'See'    },
+		{ key: 'sys_edit'   , label: 'Edit'   },
+		{ key: 'sys_delete' , label: 'Delete' }
 	];
-	@Input() data: any[] = [
-		{id:1, position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-		{id:1, position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-		{id:1, position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-		{id:1, position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-		{id:1, position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-		{id:1, position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-		{id:1, position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-		{id:1, position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-		{id:1, position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-		{id:1, position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-		{id:1, position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-		{id:1, position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-		{id:1, position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-		{id:1, position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-		{id:1, position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-		{id:1, position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-		{id:1, position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-		{id:1, position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-		{id:1, position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-		{id:1, position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
-	];
-	@Input() columnsLabels: any[] = [
-		{ key: 'name', label: 'Name' },
-		{ key: 'position', label: 'tester' },
-		{ key: 'weight', label: 'weight' },
-	];
-	@Input() columnsDisplay: string[] = [
-		'name',
-		'position',
-		'weight',
-		'sys_edit',
-		'sys_delete',
-		'sys_see'
-	];
+	// LA PROPIEDAD 'ORDER' ES NECESARIA EN EL DATA SI SE DESEA QUE SEA ORDENABLE ESTA COLUMNA
+	// por eso se descarto dejar el incremental en el html
+	@Input() data: any[] = [];
+	@Input() columnsLabels: ColumnLabel[] = [];
+	@Input() columnsDisplay: string[] = [];
 	@Input() row_x_page = [5,10,20,50, 100, 250, 500];
 
 	constructor() { }
 
 	ngOnInit() {
-		this.data;
-		this.columnsLabels;
-		this.columnsDisplay;
-		debugger;
-		this.refresh();
 	}
 	initExample(){
-
-
-
+		this.data = [
+			{order: 1, id:1,  name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+			{order: 2, id:2,  name: 'Helium', weight: 4.0026, symbol: 'He'},
+			{order: 3, id:3,  name: 'Lithium', weight: 6.941, symbol: 'Li'},
+			{order: 4, id:4,  name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+			{order: 5, id:5,  name: 'Boron', weight: 10.811, symbol: 'B'},
+			{order: 6, id:6,  name: 'Carbon', weight: 12.0107, symbol: 'C'},
+			{order: 7, id:7,  name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+			{order: 8, id:8,  name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+			{order: 9, id:9,  name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+			{order: 10, id:10,  name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+			{order: 11, id:11,  name: 'Sodium', weight: 22.9897, symbol: 'Na'},
+			{order: 12, id:12,  name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
+			{order: 13, id:13,  name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
+			{order: 14, id:14,  name: 'Silicon', weight: 28.0855, symbol: 'Si'},
+			{order: 15, id:15,  name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
+			{order: 16, id:16,  name: 'Sulfur', weight: 32.065, symbol: 'S'},
+			{order: 17, id:17,  name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
+			{order: 18, id:18,  name: 'Argon', weight: 39.948, symbol: 'Ar'},
+			{order: 19, id:19,  name: 'Potassium', weight: 39.0983, symbol: 'K'},
+			{order: 20, id:20,  name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+		];
+		this.columnsLabels = [
+			{ key: 'order', label: '#' },
+			{ key: 'name', label: 'Name' },
+			{ key: 'weight', label: 'weight' },
+			{ key: 'symbol', label: 'symbol' },
+		];
+		this.columnsDisplay = [
+			'order',
+			'name',
+			'weight',
+			'symbol',
+			'sys_edit',    // No es necesario ponerlo en el array columnsLabels
+			'sys_delete',  // No es necesario ponerlo en el array columnsLabels
+			'sys_see'      // No es necesario ponerlo en el array columnsLabels
+		];
 		/*this.dataSource.sortingDataAccessor = (item, property) => {
 			switch(property) {
 				case 'name'        : return item.name;
@@ -83,7 +80,29 @@ export class MatTableComponent implements OnInit {
 
     sortData(sort: Sort) {        
 	}
-	refresh(){
+	ngOnChanges(changes: SimpleChanges) {
+		this.init();
+	}
+	init(){
+		if(this.columnsLabels.length == 0 && this.data.length > 0){
+			const o = this.data[this.data.length-1];
+			for (const key in o) {
+				if (o.hasOwnProperty(key)) {
+					this.columnsLabels.push({key, label: key});
+				}
+			}
+		}
+		if(this.columnsDisplay.length == 0 && this.data.length > 0){
+			const o = this.data[this.data.length-1];
+			for (const key in o) {
+				if (o.hasOwnProperty(key)) {
+					this.columnsDisplay.push(key);
+				}
+			}
+			this.columnsDisplay.push('sys_see');
+			this.columnsDisplay.push('sys_edit');
+			this.columnsDisplay.push('sys_delete');
+		}
 		this.dataSource = new MatTableDataSource<any>(this.data);
 		this.dataSource.paginator = this.paginator;
 		this.dataSource.sort = this.sort;
--- src/app/safe/admin/safe-mm-out-of-line/safe-mm-out-of-line.component.ts
+++ src/app/safe/admin/safe-mm-out-of-line/safe-mm-out-of-line.component.ts
@@ -9,16 +9,35 @@ import { GlobalService } from 'src/app/core/globals/global.service';
 	styleUrls: ['./safe-mm-out-of-line.component.scss']
 })
 export class SafeMmOutOfLineComponent implements OnInit {
+	
+	// LA PROPIEDAD 'ORDER' ES NECESARIA EN EL DATA SI SE DESEA QUE SEA ORDENABLE ESTA COLUMNA
+	// por eso se descarto dejar el incremental en el html
 	tableData = [
-		{order:1,fechaOp:'mar-20',proceso:'2da Corrida MM',usuario:'Manuel Herrera',fechaMod:'01/04/2020 01:40:00 pm',estatus:'exitosa'}
+		{order:1,fechaOp:'mar-20',proceso:'2da Corrida MM',usuario:'Manuel Herrera',fechaMod:'01/04/2020 01:40:00 pm',estatus:'exitosa'},
+		{order:2,fechaOp:'mar-20',proceso:'2da Corrida MM',usuario:'Sistema'       ,fechaMod:'01/04/2020 12:40:00 pm',estatus:'exitosa'},
+		{order:3,fechaOp:'mar-20',proceso:'1ra Corrida MM',usuario:'Ivette Colin'  ,fechaMod:'01/04/2020 12:40:00 pm',estatus:'exitosa'},
+		{order:4,fechaOp:'mar-20',proceso:'1ra Corrida MM',usuario:'Sistema'       ,fechaMod:'01/04/2020 11:40:00 pm',estatus:'exitosa'},
+		{order:5,fechaOp:'mar-20',proceso:'1ra Corrida MM',usuario:'Sistema'       ,fechaMod:'01/04/2020 11:40:00 pm',estatus:'exitosa'}
 	];
 	tablaColumnsLabels=[
-		{ key: 'fechaOp', label: 'fechaOp' }
+		{ key: 'order', label: '#' },
+		{ key: 'fechaOp', label: 'fechaOp' },
+		{ key: 'proceso', label: 'proceso' },
+		{ key: 'usuario', label: 'usuario' },
+		{ key: 'fechaMod', label: 'fechaMod' },
+		{ key: 'estatus', label: 'estatus' }
 	];
 	tableColumnsDisplay: string[] = [
+		'order',
 		'fechaOp',
+		'proceso',
+		'usuario',
+		'fechaMod',
+		'estatus',
 		'sys_delete'
 	];
+	tableRow_x_page = [5,10];
+
 	constructor(
 		public globalService: GlobalService,
 		public toastr: ToastrManager
