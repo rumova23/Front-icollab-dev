53bb534e4124522dce392d250a6a36e81b09295e
Your Name <you@example.com>
2020-06-09 21:47:13 -0500

    :alien:

--- src/app/safe/admin/mining-if-c-water/mining-if-c-water.component.ts
+++ src/app/safe/admin/mining-if-c-water/mining-if-c-water.component.ts
@@ -142,7 +142,8 @@ export class MiningIFCWaterComponent implements OnInit {
 		})
 		.catch(() => {});
 	}
-	
+	onBtnCancel(){}
+	onBtnSaveUpdate(){}
 	formQuerySubmit(v){
 
 	}
--- src/app/safe/admin/mining-if-i-salaryincrease/mining-if-i-salaryincrease.component.ts
+++ src/app/safe/admin/mining-if-i-salaryincrease/mining-if-i-salaryincrease.component.ts
@@ -142,7 +142,8 @@ export class MiningIFISalaryincreaseComponent implements OnInit {
 		})
 		.catch(() => {});
 	}
-	
+	onBtnCancel(){}
+	onBtnSaveUpdate(){}
 	formQuerySubmit(v){
 
 	}
