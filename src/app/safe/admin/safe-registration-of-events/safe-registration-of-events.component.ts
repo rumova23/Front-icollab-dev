d0de9087c8aa7eb4bf18adf116d5d1ea817bbb9b
Your Name <you@example.com>
2020-07-28 13:38:35 -0500

    :alien:  timer

--- src/app/safe/admin/safe-registration-of-events/safe-registration-of-events.component.ts
+++ src/app/safe/admin/safe-registration-of-events/safe-registration-of-events.component.ts
@@ -137,7 +137,7 @@ export class SafeRegistrationOfEventsComponent implements OnInit {
 
 	ngOnInit() {
 		this.addBlock(1, '');
-		timer(10000).subscribe(()=>{this.addBlock(2, '');});// existen varios .subcribe()
+		timer(5000).subscribe(()=>{this.addBlock(2, '');});// existen varios .subcribe()
 		this.lstEvents = [];
 		this.lstRequired = [];
 		switch (this.catalogType.action) {
